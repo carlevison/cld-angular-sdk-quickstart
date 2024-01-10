1157890745c34494d726ffa1c26b43dc4de89af4
Caroline Levison <caroline.levison@cloudinary.com>
2024-01-10 15:58:41 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+// Import the Cloudinary classes.
+import {Cloudinary, CloudinaryImage} from '@cloudinary/url-gen';
+
+// Import the CloudinaryModule.
+import {CloudinaryModule} from '@cloudinary/ng';
+
+import {fill} from "@cloudinary/url-gen/actions/resize";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CloudinaryModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'my-app';
+
+export class AppComponent implements OnInit{
+  title = 'Angular Quick Start';
+  img!: CloudinaryImage;
+
+  ngOnInit() {
+
+    // Create a Cloudinary instance and set your cloud name.
+    const cld = new Cloudinary({
+      cloud: {
+        cloudName: 'demo'
+      }
+    });
+
+    // Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+    this.img = cld.image('docs/models');
+
+    // Resize to 250 x 250 pixels using the 'fill' crop mode.
+    this.img.resize(fill().width(250).height(250));
+  }
 }
